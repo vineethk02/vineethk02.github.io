183a41681712ac481fcce0114e0412953dd2c319
Vineeth Kandukuri <44505871+vineethk02@users.noreply.github.com>
2022-01-18 23:25:44 -0800

    changed code

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,17 +1,12 @@
 <?php
     $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-    $email_from = 'vinnyck02@gmail.com';
-    $email_subject = 'New Form Submission';
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-    $to = "vinnyck02@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply to: $visitor_email \r\n";
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location: index.html");
+$recipient = "vinnyck02@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 ?>
